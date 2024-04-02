4b6cd10b8060367fa31ad18f87616282fd6eafe5
Ujjwal Aryal <44752861+Azezal4@users.noreply.github.com>
2024-04-02 18:59:22 -0400

    Update jenkins-file.sh

--- jenkins-file.sh
+++ jenkins-file.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/azezal/Documents/jenkins/Django-ecommerce/venv/bin/activate
 pip install pytest
-python /home/azezal/Documents/jenkins/Django-ecommerce/venv/bin/python test.py
+/home/azezal/Documents/jenkins/Django-ecommerce/venv/bin/python test.py
